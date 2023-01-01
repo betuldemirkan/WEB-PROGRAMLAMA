4e4a02ef115316c2659267e022a209d88389359b
betuldemirkan <95471901+betuldemirkan@users.noreply.github.com>
2023-01-01 19:04:56 +0300

    Proje dosyası ekle.

---
+++ WEB PROGRAMLAMA/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("WEB_PROGRAMLAMA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WEB_PROGRAMLAMA")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğini false olarak ayarlarsanız COM bileşenleri bu 
+// derlemedeki türleri göremez. Bu derlemedeki bir türe COM'dan erişmeniz 
+// gerekiyorsa, o türde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("8efb3bea-c66a-4970-8f2f-3a94f78dc399")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilirsiniz veya Düzeltme ve Yapı Numaralarına aşağıda 
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
