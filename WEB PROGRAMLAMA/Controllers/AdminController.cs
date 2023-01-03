ae472132bba6d75652e8f65fa3af6efb6f384a0d
betuldemirkan <95471901+betuldemirkan@users.noreply.github.com>
2023-01-03 22:18:59 +0300

    Admin Templete yapıldı ve Databese first yöntemiyle veri tabanı girişşi başlandı ve proje ile veritabanı bağlantısı kuruldu

---
+++ WEB PROGRAMLAMA/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEB_PROGRAMLAMA.Models;
+
+namespace WEB_PROGRAMLAMA.Controllers
+{
+    public class AdminController : Controller
+    {
+        KurumsalDBEntities db = new KurumsalDBEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var sorgu = db.Kategoris.ToList();
+            return View(sorgu);
+        }
+    }
+}
